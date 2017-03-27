5c6a8ec4a8a7234e576e2fc72aaab300a1bbbf8f
Pavel Plutakhin <plutakhin@restream.rt.ru>
2017-03-27 17:53:28 +0400

    prepare plutkhin tests

---
+++ test/plutakhin/arrays/solution.rb
@@ -0,0 +1,14 @@
+module Plutakhin
+  module Arrays
+    class << self
+      def replace(array)
+        array
+      end
+
+      def search(array, query)
+        0
+      end
+    end
+  end
+end
+
