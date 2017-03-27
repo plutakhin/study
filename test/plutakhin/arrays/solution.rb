47965e47a49d0218115a738c75881af17a680aac
Pavel Plutakhin <plutakhin@restream.rt.ru>
2017-03-27 17:54:35 +0400

    resolve array tests

--- test/plutakhin/arrays/solution.rb
+++ test/plutakhin/arrays/solution.rb
@@ -2,13 +2,23 @@ module Plutakhin
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        array.map! { |a| a >= 0 ? max : a }
       end
 
-      def search(array, query)
-        0
+      def search(array, query, from = 0, to = array.length)
+        return -1 if from == to
+        mid = (from + to) / 2
+        value = array[mid]
+        return mid if value == query
+
+        if query < value
+          to = mid
+        else
+          from = mid + 1
+        end
+        search(array, query, from, to)
       end
     end
   end
 end
-
