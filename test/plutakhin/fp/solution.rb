eabf285c47e5d79a5965a72b5fe356030f7fc4a0
Pavel Plutakhin <plutakhin@restream.rt.ru>
2017-03-29 18:42:07 +0400

    resolve fp tests

--- test/plutakhin/fp/solution.rb
+++ test/plutakhin/fp/solution.rb
@@ -4,12 +4,19 @@ module Plutakhin
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(films)
+        ratios = films.map do |film|
+          film['rating_kinopoisk'].to_f if !film['rating_kinopoisk'].to_f.zero? && !film['country'].nil? && film['country'].split(',').count > 1
+        end . compact
+        ratios.reduce(:+) / ratios.count
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        total = 0
+        films.map do |film|
+          total += film['name'].count('и') if film['rating_kinopoisk'].to_i >= threshold
+        end
+        total
       end
     end
   end
